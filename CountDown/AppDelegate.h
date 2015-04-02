66ff8315005692e70615393b3797e1a1f94c3450
北詰 隆滋 <zume@MacBook-Pro.local>
2015-04-02 20:46:22 +0900

    Initial Commit

---
+++ CountDown/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CountDown
+//
+//  Created by 北詰 隆滋 on 2015/04/02.
+//  Copyright (c) 2015年 北詰 隆滋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CountDown/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CountDown
+//
+//  Created by 北詰 隆滋 on 2015/04/02.
+//  Copyright (c) 2015年 北詰 隆滋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
